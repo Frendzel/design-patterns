2b5336070b6f968fe383f7eb30dc66ac6e66fd47
marcinerbel <marcinerbel90@gmail.com>
2019-03-11 23:06:54 +0100

    SDA - Patterns Creational/Structural init next will be flyweight

--- patterns-buster/src/main/java/pl/sda/model/Driveable.java
+++ patterns-buster/src/main/java/pl/sda/model/Driveable.java
@@ -3,4 +3,8 @@ package pl.sda.model;
 //TODO implement generic way of informing about interface implementation
 public interface Driveable {
 
+    default void drive() {
+        System.out.println("brum brum");
+    }
+
 }
